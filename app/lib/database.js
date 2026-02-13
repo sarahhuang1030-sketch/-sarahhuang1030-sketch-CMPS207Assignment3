a12217ed1d0ae5079197b165eb84aceec7296b17
sarahhuang1030 <sarahhuang1030@gmail.com>
2026-02-12 17:53:24 -0700

    change database-connection

--- app/lib/database.js
+++ app/lib/database.js
@@ -6,12 +6,19 @@ import { createPool } from "mysql2/promise";
 
 //create connection
 const pool = createPool({
-uri: process.env.DB_CONNECTION_STRING, // <-- from Key Vault
+  // uri: process.env.DB_CONNECTION_STRING, // <-- from Key Vault
+
   // host: process.env.DB_HOST || "localhost",
   // user: process.env.DB_USER || "root",
   // password: process.env.DB_PASSWORD || "Sarah923*",
   // database: process.env.DB_NAME || "oosd_webappdev",
   // port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
+  port: process.env.DB_PORT,
+  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
